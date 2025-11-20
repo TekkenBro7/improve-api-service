61efceea35db33a8bd0883016b40daad40333e45
Maxim <maks.kreet@mail.ru>
2025-11-20 14:04:41 +0300

    Add background task to generate report

--- src/api/v1/analytics.py
+++ src/api/v1/analytics.py
@@ -2,17 +2,18 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.session import get_async_session
-from src.services.analytics_service import AnalyticsService
+from src.worker.tasks import generate_weekly_report
 
 router = APIRouter()
 
 
 @router.get(
     "/transactions",
-    response_model=list[dict],
+    response_model=dict[str, str],
     status_code=status.HTTP_200_OK,
 )
 async def get_transaction_analysis(
     session: AsyncSession = Depends(get_async_session),
-) -> list[dict]:
-    return await AnalyticsService(session).get_weekly_analysis()
+) -> dict[str, str]:
+    generate_weekly_report.send()
+    return {"message": "OK"}
